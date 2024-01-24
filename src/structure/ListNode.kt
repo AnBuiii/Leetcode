533dd072f3f405485607d1b9c7ea186589ce3a12
Bùi Lê Hoài An <builehoaian2002@gmail.com>
2024-01-24 23:09:08 +0700

    list node converter

--- src/structure/ListNode.kt
+++ src/structure/ListNode.kt
@@ -2,12 +2,31 @@ package structure
 
 class ListNode(val `val`: Int) {
     var next: ListNode? = null
-}
 
-fun ListNode?.getString(): String {
-    return if (this != null) {
-        "${this.next.getString()}${this.`val`}"
-    } else {
-        ""
+    override fun toString(): String {
+        return "${this.next.toString()}${this.`val`}"
+    }
+
+    companion object {
+        fun fromArray(array: IntArray): ListNode? {
+            if (array.isEmpty()) return null
+            val root = ListNode(0)
+            var temp: ListNode? = root
+            for (element in array) {
+                temp?.next = ListNode(element)
+                temp = temp?.next
+            }
+            return root.next
+        }
+
+        fun ListNode?.toArray(): IntArray {
+            val result = mutableListOf<Int>()
+            var temp: ListNode? = this
+            while (temp != null) {
+                result.add(temp.`val`)
+                temp = temp.next
+            }
+            return result.toIntArray()
+        }
     }
 }
--- test/Remove_Nth_Node_From_End_of_List/Remove_Nth_Node_From_End_of_List_Test.kt
+++ test/Remove_Nth_Node_From_End_of_List/Remove_Nth_Node_From_End_of_List_Test.kt
@@ -1,15 +1,17 @@
 package Remove_Nth_Node_From_End_of_List
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import structure.ListNode
+import structure.ListNode.Companion.toArray
 
 class Remove_Nth_Node_From_End_of_List_Test {
 
     @Test
     fun removeNthFromEnd() {
-
         val solution = Solution()
+        val head = ListNode.fromArray(intArrayOf(1, 2, 3, 4, 5))
+        assertArrayEquals(intArrayOf(1, 2, 3, 5), solution.removeNthFromEnd(head, 2).toArray())
     }
 
 }
